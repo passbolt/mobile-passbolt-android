461b72be2fc1cd6e76c336110b3e8daf17ca4393
Marcin Michałek <marcin.michalek@miquido.com>
2021-06-08 09:44:02 +0200

    PAS-114: Add autofill enabled dialog

---
+++ features/autofill/src/main/java/com/passbolt/mobile/android/feature/autofill/enabled/AutofillEnabledDialog.kt
@@ -0,0 +1,74 @@
+package com.passbolt.mobile.android.feature.autofill.enabled
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.passbolt.mobile.android.common.extension.setDebouncingOnClick
+import com.passbolt.mobile.android.feature.autofill.R
+import com.passbolt.mobile.android.feature.autofill.databinding.DialogAutofillEnabledBinding
+
+/**
+ * Passbolt - Open source password manager for teams
+ * Copyright (c) 2021 Passbolt SA
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License (AGPL) as published by the Free Software Foundation version 3.
+ *
+ * The name "Passbolt" is a registered trademark of Passbolt SA, and Passbolt SA hereby declines to grant a trademark
+ * license to "Passbolt" pursuant to the GNU Affero General Public License version 3 Section 7(e), without a separate
+ * agreement with Passbolt SA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see GNU Affero General Public License v3 (http://www.gnu.org/licenses/agpl-3.0.html).
+ *
+ * @copyright Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link https://www.passbolt.com Passbolt (tm)
+ * @since v1.0
+ */
+class AutofillEnabledDialog : DialogFragment() {
+
+    private var listener: Listener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.FullscreenDialogTheme)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DialogAutofillEnabledBinding.inflate(inflater)
+        setupListeners(binding)
+        return binding.root
+    }
+
+    private fun setupListeners(binding: DialogAutofillEnabledBinding) {
+        with(binding) {
+            closeButton.setDebouncingOnClick { dismiss() }
+            goToAppButton.setDebouncingOnClick { listener?.goToAppClick() }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = when {
+            activity is Listener -> activity as Listener
+            parentFragment is Listener -> parentFragment as Listener
+            else -> error("Parent must implement ${Listener::class.java.name}")
+        }
+    }
+
+    override fun onDetach() {
+        listener = null
+        super.onDetach()
+    }
+
+    interface Listener {
+        fun goToAppClick()
+    }
+}
