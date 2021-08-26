7833a90b9182717e33c634a197593e810210436b
Marcin Michałek <marcin.michalek@miquido.com>
2021-08-26 14:12:17 +0200

    PAS-264: Store resource types and fields in database

--- core/commonresource/src/main/java/com/passbolt/mobile/android/core/commonresource/ResourceListUiModel.kt
+++ core/commonresource/src/main/java/com/passbolt/mobile/android/core/commonresource/ResourceListUiModel.kt
@@ -26,6 +26,7 @@ import kotlinx.parcelize.Parcelize
  * @link https://www.passbolt.com Passbolt (tm)
  * @since v1.0
  */
+
 sealed class ResourceListUiModel : Parcelable {
     @Parcelize
     class Data(
