6721b41515d9b9e1232f2a7cfb90b3f484334d06
Marcin Michałek <marcin.michalek@miquido.com>
2021-09-22 12:49:40 +0200

    PAS-333: Handle login open pgp errors

--- features/authentication/src/test/java/com/passbolt/mobile/android/feature/authentication/auth/challenge/ChallengeDecryptorTest.kt
+++ features/authentication/src/test/java/com/passbolt/mobile/android/feature/authentication/auth/challenge/ChallengeDecryptorTest.kt
@@ -1,12 +1,13 @@
 package com.passbolt.mobile.android.feature.authentication.auth.challenge
 
+import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.whenever
+import com.passbolt.mobile.android.feature.authentication.challengeTestModule
 import com.passbolt.mobile.android.feature.authentication.getPrivateKeyUseCase
 import com.passbolt.mobile.android.feature.authentication.openPgp
-import com.passbolt.mobile.android.feature.authentication.challengeTestModule
-import com.passbolt.mobile.android.feature.authentication.auth.challenge.ChallengeDecryptor
+import com.passbolt.mobile.android.gopenpgp.exception.OpenPgpException
 import com.passbolt.mobile.android.storage.usecase.privatekey.GetPrivateKeyUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -37,14 +38,37 @@ class ChallengeDecryptorTest : KoinTest {
         whenever(openPgp.decryptVerifyMessageArmored(eq(publicKey), eq(privateKey), any(), any())).thenReturn(
             challenge.toByteArray()
         )
+
+
         val result = challengeDecryptor.decrypt(
             publicKey, "pass".toByteArray(), "userId", "challenge"
         )
-        assertEquals("access_token", result.accessToken)
-        assertEquals("verify_token", result.verifyToken)
-        assertEquals("refresh_token", result.refreshToken)
-        assertEquals("domain", result.domain)
-        assertEquals("1.0", result.version)
+        assertThat(result).isInstanceOf(ChallengeDecryptor.Output.DecryptedChallenge::class.java)
+        val decryptedChallenge = (result as ChallengeDecryptor.Output.DecryptedChallenge).challenge
+        assertEquals("access_token", decryptedChallenge.accessToken)
+        assertEquals("verify_token", decryptedChallenge.verifyToken)
+        assertEquals("refresh_token", decryptedChallenge.refreshToken)
+        assertEquals("domain", decryptedChallenge.domain)
+        assertEquals("1.0", decryptedChallenge.version)
+    }
+
+    @Test
+    fun `challenge value is correct when decryption failure`() = runBlockingTest {
+        val privateKey = "private_key"
+        val publicKey = "public_key"
+        val exceptionMessage = "message"
+        whenever(getPrivateKeyUseCase.execute(any())).thenReturn(GetPrivateKeyUseCase.Output(privateKey))
+        whenever(openPgp.decryptVerifyMessageArmored(any(), any(), any(), any()))
+            .thenThrow(OpenPgpException(exceptionMessage))
+
+
+
+        val result = challengeDecryptor.decrypt(
+            publicKey, "pass".toByteArray(), "userId", "challenge"
+        )
+        assertThat(result).isInstanceOf(ChallengeDecryptor.Output.DecryptionError::class.java)
+        val decryptionError = (result as ChallengeDecryptor.Output.DecryptionError)
+        assertThat(decryptionError.message).isEqualTo(exceptionMessage)
     }
 
 }
