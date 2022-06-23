2ee16ad7aee573c5081c372406e8e2550f5775cb
Marcin Michałek <marcin.michalek@miquido.com>
2022-06-23 12:21:14 +0200

    MOB-463: QA debug features: app version on icon & allow screen shots on debuggable app

--- core/security/src/main/java/com/passbolt/mobile/android/core/security/flagsecure/FlagSecureSetter.kt
+++ core/security/src/main/java/com/passbolt/mobile/android/core/security/flagsecure/FlagSecureSetter.kt
@@ -2,6 +2,7 @@ package com.passbolt.mobile.android.core.security.flagsecure
 
 import android.app.Activity
 import android.view.WindowManager
+import com.passbolt.mobile.android.core.security.BuildConfig
 
 /**
  * Passbolt - Open source password manager for teams
@@ -29,15 +30,19 @@ import android.view.WindowManager
 class FlagSecureSetter {
 
     fun set(activity: Activity) {
-        activity.window.setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE
-        )
+        if (!BuildConfig.DEBUG) {
+            activity.window.setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE
+            )
+        }
     }
 
     fun remove(activity: Activity) {
-        activity.window.clearFlags(
-            WindowManager.LayoutParams.FLAG_SECURE
-        )
+        if (!BuildConfig.DEBUG) {
+            activity.window.clearFlags(
+                WindowManager.LayoutParams.FLAG_SECURE
+            )
+        }
     }
 }
