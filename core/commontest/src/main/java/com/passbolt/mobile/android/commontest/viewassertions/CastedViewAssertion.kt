d024f6d7f7bafb13aa4fb8ee0c43d2d213d47e69
Grzegorz Woźniczka <grzegorz.wozniczka@miquido.com>
2022-09-08 11:11:31 +0200

    test(MOB-548) Add Test for Connecting explanation screen

---
+++ core/commontest/src/main/java/com/passbolt/mobile/android/commontest/viewassertions/CastedViewAssertion.kt
@@ -0,0 +1,17 @@
+package com.passbolt.mobile.android.commontest.viewassertions
+
+import android.view.View
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+
+class CastedViewAssertion<CAST_TO : View>(val assertion: (CAST_TO) -> Boolean) : ViewAssertion {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun check(view: View?, noViewFoundException: NoMatchingViewException?) {
+        try {
+            assert(assertion(view as CAST_TO))
+        } catch (exception: ClassCastException) {
+            throw AssertionError("Supplied view (${view?.javaClass?.name}) cannot be casted to used type.")
+        }
+    }
+}
