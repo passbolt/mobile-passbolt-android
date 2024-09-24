48be97b7a1ac778398943c8a2934da4f197f9b66
Marcin Michałek <marcin.michalek@miquido.com>
2024-09-24 05:39:40 +0000

    MOB-1994: Fix missing help dialog on welcome screen

--- core/common/src/main/java/com/passbolt/mobile/android/common/dialogs/Dialogs.kt
+++ core/common/src/main/java/com/passbolt/mobile/android/common/dialogs/Dialogs.kt
@@ -163,3 +163,25 @@ fun weakPasswordAlertDialog(context: Context, onProceed: () -> Unit) =
         .setNegativeButton(LocalizationR.string.proceed) { _, _ -> onProceed() }
         .setCancelable(false)
         .create()
+
+fun setupExitConfirmationDialog(context: Context, onExit: () -> Unit) =
+    AlertDialog.Builder(context)
+        .setTitle(LocalizationR.string.are_you_sure)
+        .setMessage(LocalizationR.string.scan_qr_exit_confirmation_dialog_message)
+        .setPositiveButton(LocalizationR.string.cancel) { _, _ -> }
+        .setNegativeButton(LocalizationR.string.stop_scanning) { _, _ -> onExit() }
+        .create()
+
+fun qrCodesInformationDialog(context: Context) =
+    AlertDialog.Builder(context)
+        .setTitle(LocalizationR.string.scan_qr_exit_information_dialog_title)
+        .setMessage(LocalizationR.string.scan_qr_exit_information_dialog_message)
+        .setPositiveButton(LocalizationR.string.got_it) { _, _ -> }
+        .create()
+
+fun howToCreateAccountDialog(context: Context) =
+    AlertDialog.Builder(context, com.passbolt.mobile.android.core.ui.R.style.AlertDialogTheme)
+        .setTitle(LocalizationR.string.welcome_create_account_dialog_title)
+        .setMessage(LocalizationR.string.welcome_create_account_dialog_message)
+        .setPositiveButton(LocalizationR.string.got_it) { _, _ -> }
+        .create()
