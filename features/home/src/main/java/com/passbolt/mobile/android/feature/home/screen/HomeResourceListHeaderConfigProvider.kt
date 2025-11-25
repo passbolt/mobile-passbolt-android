88be6a607e2f4118a9e881dfa3a0eebf4904f3da
Marcin Michałek <marcin.michalek@miquido.com>
2025-11-25 09:07:24 +0100

    MOB-3161: Hide *other* header if no suggested resources in autofill

--- features/home/src/main/java/com/passbolt/mobile/android/feature/home/screen/HomeResourceListHeaderConfigProvider.kt
+++ features/home/src/main/java/com/passbolt/mobile/android/feature/home/screen/HomeResourceListHeaderConfigProvider.kt
@@ -63,7 +63,7 @@ internal fun getHeaderConfig(
             isInCurrentFolderSectionVisible = false,
             isInSubFoldersSectionVisible = false,
             isOtherItemsSectionVisible =
-                !loadState.areAllEmpty && showSuggestedModel is Show,
+                !loadState.areAllEmpty && showSuggestedModel is Show && !suggestedResources.itemSnapshotList.isEmpty(),
             isSuggestedSectionVisible = !suggestedResources.itemSnapshotList.isEmpty(),
             currentFolderName = currentFolderName,
             areAllSectionsEmpty = loadState.areAllEmpty && !loadState.isAnyLoading,
